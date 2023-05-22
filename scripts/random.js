23e513ac30d551b16f5fbf53ed674f755b77d6b8
Acrata <hxtr227@gmail.com>
2023-05-22 15:18:20 +0200

    random mecanism

--- scripts/random.js
+++ scripts/random.js
@@ -12,7 +12,6 @@ function shuffleArray(array) {
   while (currentIndex !== 0) {
     // Pick a remaining element
     randomIndex = Math.floor(Math.random() * currentIndex);
-    console.log(currentIndex, 'ci', randomIndex);
     currentIndex -= 1;
 
     // Swap it with the current element
@@ -24,94 +23,119 @@ function shuffleArray(array) {
   return array;
 }
 
-// console.log(result, 'res')
 
-const sa = shuffleArray(movies.movies)
-const toplay = sa.slice(0,5)
-console.log(sa,  'sm', toplay)
+// Function to create a movie card element
+function createMovieCard(item) {
+  const articleCard = document.createElement('article');
+  articleCard.classList.add('card');
+  articleCard.textContent = item.name;
+  articleCard.dataset.movieId = item.id; // Set data attribute
+  return articleCard;
+}
 
-toplay.map( (item, i) => {
-  const movie = document.createElement('p')
-  const articleCard = document.createElement('article')
-  movie.textContent = item.name
-  articleCard.classList.add('card')
-  articleCard.textContent = item.name
-  articleCard.id = item.id
-  list.appendChild(articleCard)
-  movie.classList.add('movie-name')
-  movie.id = `movie-${item.id}`
-  // movie.style.opacity = 0
-  randomContainer.appendChild(movie)
-  setTimeout(() => {
-    // movie.removeAttribute('id')
-  }, 10)
-  // movie.setAttribute('id', `animation-title`)
-})
+// Function to create a movie name element
+function createMovieNameElement(item) {
+  const movie = document.createElement('p');
+  movie.textContent = item.name;
+  movie.classList.add('movie-name');
+  movie.dataset.movieId = item.id; // Set data attribute
+  return movie;
+}
+
+// Shuffle the movies array
+const shuffledArray = shuffleArray(movies.movies);
+
+// Select the top 5 movies from the shuffled array
+const topMovies = shuffledArray.slice(0, 5);
+
+// Iterate over the top movies and create movie cards and movie name elements
+topMovies.forEach((item) => {
+  const movieCard = createMovieCard(item);
+  const movieNameElement = createMovieNameElement(item);
+
+  // Append the movie card and movie name element to the desired containers
+  list.appendChild(movieCard);
+  randomContainer.appendChild(movieNameElement);
+
+});
 
 
  const words = document.querySelectorAll('.movie-name');
  const cards = document.querySelectorAll('.card');
-console.log(words, 'words')
-    let currentWord = 0;
-    let animationPlayState = 'play'
-    function animateWords() {
-      words[currentWord].classList.add('visible');
-        cards[currentWord].style.borderColor = 'gold'
-      const listItem = document.getElementById('item')
-      const timing = setTimeout(() => {
-        words[currentWord].classList.remove('visible');
-        cards[currentWord].style.borderColor = 'black'
-        currentWord = (currentWord + 1) % words.length;
-        // console.log(currentWord)
-        if(animationPlayState === 'play'){
-          animateWords();
-        }
-      }, 200);
-      setTimeout(() => {
-        clearTimeout(timing)
-        animationPlayState = 'paused'
-        console.log('is time?', words[currentWord])
-        words[currentWord].setAttribute('data-veredict', 'winner')
-        words[currentWord].classList.add('visible');
-        cards[currentWord].style.borderColor = 'gold'
-        cards[currentWord].style.backgroundColor = 'black'
-        cards[currentWord].style.color = 'white'
-      }, Math.floor(Math.random() * (20000 - 3000 + 1) + 3000))
-      // }, 2000)
+const ganadoraRespuesta = document.getElementById('respuesta-ganadora')
+const respuestaModal = document.getElementById('respuesta-dialog')
+const closeModal = document.getElementById('close-modal')
+ let currentWord = 0;
+let animationPlayState = 'play';
+let timing;
+let animationStopped = false;
+// console.log(cards, 'cards', shuffleArray(new (cards)))
+function animateWords() {
+  words[currentWord].classList.add('visible');
+  cards[currentWord].style.borderColor = 'gold';
 
+  timing = setTimeout(() => {
+    words[currentWord].classList.remove('visible');
+    cards[currentWord].style.borderColor = 'black';
+  
+    // console.log(words[currentWord].dataset.movieId, 'data mid', cards);
+    currentWord = (currentWord + 1) % words.length;
+    if (animationPlayState === 'play' && !animationStopped) {
+      animateWords();
     }
+  }, 200);
+}
 
-    animateWords();
-      const getListItem = document.querySelector('[data-veredict]')
-      console.log(getListItem, 'glijj');
+function stopAnimation() {
+  animationStopped = true;
+}
 
+function resetAnimation() {
+  currentWord = 0;
+  animationStopped = true;
+  
+  animationPlayState = 'play'
+  // clearTimeout(timing)
+  animateWords();
+}
 
+function handleAnimationEnd() {
+  clearTimeout(timing);
+  cards[currentWord].setAttribute('data-veredict', 'winner');
+  console.log(words[currentWord].textContent)
+  ganadoraRespuesta.innerText = words[currentWord].textContent
+  words[currentWord].classList.add('visible');
+  cards[currentWord].style.borderColor = 'gold';
+  cards[currentWord].style.backgroundColor = 'black';
+  cards[currentWord].style.color = 'white';
+  setTimeout(() => {
+    respuestaModal.showModal()
+  }, 1600)
+}
 
-// const  elementsToAnimate = gsap.utils.toArray('.movie-name')
-// const tl = gsap.timeline({ repeat: -1, repeatDelay: .3, paused: true }, "-=0.7")
-// const backdrop = document.getElementById('backdrop')
-// const vamosBtn = document.getElementById('vamos-mobile')
-// const colors = ['#ff0000', '#00ff00', '#0000ff', '#ffff00', '#ff00ff', '#00ffff'];
+words[currentWord].addEventListener('animationend', () => {
+  // handleAnimationEnd();
+  // resetAnimation()
+  // enableButton();
+});
 
+// Start the animation
+const vamosBtn = document.getElementById('vamos-mobile')
+vamosBtn.addEventListener('click', () => {
+const stopTime = Math.floor(Math.random() * (10000 - 3000 + 1) + 3000);
+  animateWords();
+setTimeout(() => {
+  stopAnimation();
+  console.log('sett')
+  handleAnimationEnd();
+}, stopTime);
+})
+const reset = document.getElementById('reiniciar')
+reset.addEventListener('click', () => {
+  resetAnimation()
+})
 
-// elementsToAnimate.forEach( (elem, i) => {
-//   const randItemIndex = gsap.utils.random(elementsToAnimate)
-//   console.log(randItemIndex.textContent)
-//     // tl.fromTo(elem, {scale: 5, autoAlpha :0}, { scale: 0, autoAlpha: 1, delay: 0.5 + (0.5 * i), repeat: -1})
-//     tl.to(randItemIndex, {scale: 5, autoAlpha: 0, y: -12 })
-//     // tl.to(backdrop, {autoAlpha: 1}).to(backdrop, {autoAlpha: 0})
-//   tl.to('article:nth-child(2)', {border: '2px solid #D4B42D'})
-//     // tl.timeScale(0.6 / i * 25  )
-//   })
-
-
-// vamosBtn.addEventListener('click', () => {
-//   console.log(tl.isActive())
-//   if(tl.isActive()) {
-//     console.log('dd')
-//     tl.pause()
-//   }
-//   tl.play()
-//  
-// })
-
+closeModal.addEventListener('click', () => {
+  respuestaModal.close()
+  // cards[currentWord].remove()
+})
